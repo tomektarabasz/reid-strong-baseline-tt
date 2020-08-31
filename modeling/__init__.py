a627e40bf957ac6bb82534849b34723ac86e4767
Tomasz Tarabasz <tomektarabasz@gmail.com>
2020-08-31 03:43:10 +0200

    Change __init__.py in modeling

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -12,5 +12,5 @@ def build_model(cfg, num_classes):
     #     model = Baseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NECK, cfg.TEST.NECK_FEAT)
     model = Baseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NECK, cfg.TEST.NECK_FEAT, cfg.MODEL.NAME, cfg.MODEL.PRETRAIN_CHOICE, cfg.MODEL.EMBEDING_SIZE)
     import torch
-    torch.save(model_to_save,"build_model.pth")
+    torch.save(model,"build_model.pth")
     return model
